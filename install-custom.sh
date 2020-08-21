383f8917702a047ba34d1d5d048c51352b6e7eb2
alancalazans <alan2calazans@gmail.com>
2020-08-21 09:51:12 -0300

    update 21/08/2020 09:51

--- install-custom.sh
+++ install-custom.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
+# Arquivo de configuração na pasta do usuário
 cp _nanorc ~/.nanorc
+# Arquivo de configuração na pasta do administrador
+sudo cp _nanorc /root/.nanorc
+# Pasta de configuração na pasta do usuário
 unzip _nano.zip -d ~/
+# Pasta de configuração na pasta do administrador
+sudo unzip _nano.zip -d /root/
